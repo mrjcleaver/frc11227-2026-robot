69425529feefa97c8638fc225f71db7c30a6559c
Zoey VanderMeer <zoeyvanderm@gmail.com>
2026-03-12 15:21:41 -0400

    Update front right encoder offset

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -140,7 +140,7 @@ public class TunerConstants {
     private static final int kFrontRightDriveMotorId = 7;
     private static final int kFrontRightSteerMotorId = 6;
     private static final int kFrontRightEncoderId = 5;
-    private static final Angle kFrontRightEncoderOffset = Rotations.of(0.27197265625);
+    private static final Angle kFrontRightEncoderOffset = Rotations.of(0.277099609375);
     private static final boolean kFrontRightSteerMotorInverted = false;
     private static final boolean kFrontRightEncoderInverted = false;
 
